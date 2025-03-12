a829db86f5c213c2713a8e6c3521ec959fb8047b
rlbean <rbean@wsd1.org>
2025-03-12 09:50:22 -0500

    Added explanation to Error Handling file.

--- errorHandling.py
+++ errorHandling.py
@@ -1,17 +1,23 @@
-def get_student_grade():
-    while True:
-        try:
-            grade = float(input("Enter student grade (0-100): "))
-            if 0<= grade <= 100:
-                return grade
-            else: 
-                print("Grade must be between 0 and 100.")
-        except ValueError:
-            print("Please enter a numeric value.")
+# Try-Except with specific error messages
+try:
+    #Get user input
+    num1 = int(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 / num2
+# user enters a string value instead of number
+except ValueError:
+    print("Error: Please enter valid numbers.")
+#user enters 0 for their second number
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero (0).")
+else:
+    # if the code works --> show the result
+    print(f"The result is: {result}") #move from the try section
+finally:
+    #indicate that the try-except is complete
+    print("Calculation attempt completed.")
 
-print(f"The student grade is {get_student_grade()}")
-
-"""
+# Real World Example - Withdraw money from bank
 class InsufficientFundsError(Exception):
     '''
     Exception raised when a withdrawal would result in a negative balance
@@ -23,9 +29,11 @@ def withdraw(balance, amount):
         raise InsufficientFundsError("Not enough funds available")
     return balance - amount
 
-print(withdraw(500, 200))
-"""
-'''
+#first one crashes/stops program because it didn't pass the test
+#print(withdraw(50, 200)) # I only have 50 so I CAN'T take out 200
+print(withdraw(500, 200)) # i have 500 so I can take out 200
+
+#Raise - allows programmer to create their own error messaging
 def set_age(age):
     if age < 0:
         raise ValueError("Age cannot be negative")
@@ -33,21 +41,30 @@ def set_age(age):
         raise ValueError("Age is unrealistically high!")
     return f"Age set to {age}"
 
+#print(set_age(-4)) #would crash the program -- Age cannot be negative
+#print(set_age(121)) #would crash the program -- Age is unrealistically high
+print(set_age(50)) #code works
 
-print(set_age(121))
-'''
-'''
-try:
-    #Get user input
-    num1 = int(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1 / num2
-except ValueError:
-    print("Error: Please enter valid numbers.")
-except ZeroDivisionError:
-    print("Error: Cannot divide by zero (0).")
-else:
-    print(f"The result is: {result}") #move from the try section
-finally:
-    print("Calculation attempt completed.")'
-'''
+
+# Real World example: Student Grade Entry
+# Try Except in a loop
+#function to enter a grade
+def get_student_grade():
+    #Continue to run this until you have entered a valid grade
+    while True: 
+        #try-except to catch any errors
+        try:
+            grade = float(input("Enter student grade (0-100): "))
+            #if the grade is between 0 adn 100 --> it is good
+            if 0<= grade <= 100:
+                return grade
+            #otherwise - send message to fix it and run again. 
+            else: 
+                print("Grade must be between 0 and 100.")
+        #User trys to enter a string or non-number value
+        except ValueError:
+            #explain why their entry was wrong. 
+            print("Please enter a numeric value.")
+
+# Requests a student grade and outputs this message
+print(f"The student grade is {get_student_grade()}")
